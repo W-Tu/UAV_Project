fababda8432d913a0a3e451a016e12285cbc022e
W-Tu <83734952+W-Tu@users.noreply.github.com>
2021-09-06 15:16:49 +1200

    Add files via upload

---
+++ custom-aodv-routing-protocol-module.h
@@ -0,0 +1,18 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_CUSTOM_AODV_ROUTING_PROTOCOL
+    
+
+// Module headers:
+#include "custom-aodv-dpd.h"
+#include "custom-aodv-id-cache.h"
+#include "custom-aodv-neighbor.h"
+#include "custom-aodv-packet.h"
+#include "custom-aodv-routing-protocol-helper.h"
+#include "custom-aodv-routing-protocol.h"
+#include "custom-aodv-rqueue.h"
+#include "custom-aodv-rtable.h"
+#endif
